1250a621d18cc4413f971f9fc7b7e1c6f41963df
4me7h <4573r10n@gmail.com>
2024-03-08 15:13:42 -0600

    style: animations about

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,8 @@ import Col from 'react-bootstrap/Col';
 
 import Gridimg from './Gridimg';
 
+import { Fade } from 'react-awesome-reveal';
+
 export default function About() {
   return (
     <section className="about">
@@ -12,9 +14,12 @@ export default function About() {
         <Row>
           <Col>
             <div className="about__container">
+              <Fade direction='down'>
               <div className="about__header">
                 <h3 className="text--subtitle text-center">Biografía</h3>
               </div>
+              </Fade>
+              <Fade cascade direction="left">
               <div className="about__content">
                 <p className='about__paragraph text-body'>
                   <strong>Justo Tomás Hernández</strong> es un dedicado líder comunitario con profundos vínculos en Chiapas. Graduado con honores en Ciencias Políticas por la <a href="https://www.unach.mx/">Universidad Autónoma de Chiapas</a>, Justo ha dedicado su vida a servir a su comunidad desde una edad temprana.
@@ -23,6 +28,7 @@ export default function About() {
                   Proveniente de una familia comprometida con la justicia social, Justo ha trabajado incansablemente en proyectos locales para mejorar las condiciones de vida de sus conciudadanos. Su trayectoria profesional incluye roles destacados en organizaciones no gubernamentales, donde ha liderado iniciativas exitosas para promover la igualdad de oportunidades y la participación ciudadana.
                 </p>
               </div>
+              </Fade>
               {/* <div className="gallery">
                 <ul className="gallery__container">
                   <li className="gallery__item"></li>
